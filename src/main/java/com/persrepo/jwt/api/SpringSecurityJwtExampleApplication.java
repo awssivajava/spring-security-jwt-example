70b5cf45f43472c04c5d49563046c69089cb15d1
awssivajava <sivag.usa@gmail.com>
2025-11-09 17:35:09 -0600

    initial commit

--- src/main/java/com/persrepo/jwt/api/SpringSecurityJwtExampleApplication.java
+++ src/main/java/com/persrepo/jwt/api/SpringSecurityJwtExampleApplication.java
@@ -1,7 +1,7 @@
-package com.javatechie.jwt.api;
+package com.persrepo.jwt.api;
 
-import com.javatechie.jwt.api.entity.User;
-import com.javatechie.jwt.api.repository.UserRepository;
+import com.persrepo.jwt.api.entity.User;
+import com.persrepo.jwt.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +19,7 @@ public class SpringSecurityJwtExampleApplication {
     @PostConstruct
     public void initUsers() {
         List<User> users = Stream.of(
-                new User(101, "javatechie", "password", "javatechie@gmail.com"),
+                new User(101, "persrepo", "password", "persrepo@gmail.com"),
                 new User(102, "user1", "pwd1", "user1@gmail.com"),
                 new User(103, "user2", "pwd2", "user2@gmail.com"),
                 new User(104, "user3", "pwd3", "user3@gmail.com")
